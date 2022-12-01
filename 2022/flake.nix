26631275f9f7bfbce53d160563f6d80133bfb96b
Luke Worth <luke.worth@bellroy.com>
2022-12-02 10:11:45 +1100

    Add flake for 2022

---
+++ 2022/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Advent of Code 2022";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.idris2 pkgs.rlwrap ];
+        };
+      }
+    );
+}
