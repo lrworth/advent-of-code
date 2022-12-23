b797040d77bb876ef79e76162212f077844e1fea
Luke Worth <luke.worth@bellroy.com>
2022-12-24 01:40:25 +1100

    2022/16: Failing even more

--- 2022/16/src/Aoc.hs
+++ 2022/16/src/Aoc.hs
@@ -32,8 +32,8 @@ import Data.List qualified as List
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.List.NonEmpty qualified as NEL
 import Data.List.Split (chunksOf, splitOn)
-import Data.Map (Map)
-import Data.Map qualified as Map
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
 import Data.Maybe (catMaybes, fromMaybe, mapMaybe, maybeToList)
 import Data.Semigroup
 import Data.Set (Set)
@@ -87,8 +87,15 @@ readValveFile path = do
   Just valves <- pure . fmap mconcat $ traverse parseValveLine ls
   pure valves
 
-distance :: Map Valve ValveData -> Valve -> Valve -> Int
-distance valveMap from to = bfs 0 [from]
+distance :: Map Valve ValveData -> Valve -> Valve -> State S Int
+distance valveMap from to = do
+  dM <- gets $ Map.lookup (from, to) . distanceMemo
+  case dM of
+    Just d -> pure d
+    Nothing -> do
+      let d = bfs 0 [from]
+      modify $ \s -> s {distanceMemo = Map.insert (from, to) d $ distanceMemo s}
+      pure d
   where
     bfs d vs =
       if to `elem` vs
@@ -109,56 +116,83 @@ totalFlowRate valveMap =
 annotate :: Show a => String -> a -> a
 annotate s a = traceShow (s <> ": " <> show a) a
 
-cost :: Map Valve ValveData -> Valve -> Set Valve -> Set Valve -> Int
-cost valveMap v goalVs = \avoid ->
-  let thisFlowRate = flowRate $ valveMap Map.! v
-   in -- annotate (show v <> ", " <> show goalVs) $
-      if Set.null goalVs
-        then thisFlowRate
-        else
-          minimum . fmap snd $
+memoPure :: Valve -> Set Valve -> Int -> State S Int
+memoPure v goalVs n = do
+  modify $ \s -> s {costMemo = Map.insert (v, goalVs) n $ costMemo s}
+  -- traceShowM =<< gets (Map.size . costMemo)
+  pure n
+
+data S = S
+  { costMemo :: Map (Valve, Set Valve) Int,
+    distanceMemo :: Map (Valve, Valve) Int
+  }
+
+cost :: Map Valve ValveData -> Valve -> Set Valve -> Set Valve -> State S Int
+cost valveMap v goalVs avoid = do
+  -- First see if it's been visited before
+  prior <- gets $ Map.lookup (v, goalVs) . costMemo
+  case prior of
+    Just i -> pure i
+    Nothing ->
+      memoPure v goalVs =<< do
+        let thisFlowRate = flowRate $ valveMap Map.! v
+        if Set.null goalVs
+          then pure thisFlowRate
+          else do
             -- Cost of opening the current valve then traversing everything
-            toList
-              ( Set.map
-                  ( \goalV ->
-                      let remaining = Set.delete goalV goalVs
-                       in ( goalV,
-                            thisFlowRate
-                              + ((1 + distance valveMap v goalV) * totalFlowRate valveMap goalVs)
-                              + cost
-                                valveMap
-                                goalV
-                                remaining
-                                avoid
-                          )
-                  )
-                  goalVs
-              )
-              -- Cost of skipping the current valve and traversing from somewhere else
-              ++ catMaybes
-                ( toList
-                    ( Set.map
-                        ( \goalV ->
-                            if Set.member goalV avoid
-                              then Nothing
-                              else
-                                Just $
-                                  let remaining = Set.delete goalV $ Set.insert v goalVs
-                                   in ( goalV,
-                                        distance valveMap v goalV * totalFlowRate valveMap (Set.insert v goalVs)
-                                          + cost
-                                            valveMap
-                                            goalV
-                                            remaining
-                                            (Set.insert v avoid)
-                                      )
-                        )
-                        goalVs
-                    )
+            costOpenCurrent <-
+              traverse
+                ( \goalV -> do
+                    let remaining = Set.delete goalV goalVs
+                    c <-
+                      cost
+                        valveMap
+                        goalV
+                        remaining
+                        avoid
+                    d <- distance valveMap v goalV
+                    pure
+                      ( thisFlowRate
+                          + ((1 + d) * totalFlowRate valveMap goalVs)
+                          + c
+                      )
                 )
+                (toList goalVs)
+            -- Cost of skipping the current valve and traversing from somewhere else
+            costSkipCurrent <-
+              traverse
+                ( \goalV ->
+                    if Set.member goalV avoid
+                      then pure Nothing
+                      else do
+                        let remaining = Set.delete goalV $ Set.insert v goalVs
+                        c <-
+                          cost
+                            valveMap
+                            goalV
+                            remaining
+                            (Set.insert v avoid)
+                        d <- distance valveMap v goalV
+                        pure $
+                          Just
+                            ( d * totalFlowRate valveMap (Set.insert v goalVs)
+                                + c
+                            )
+                )
+                (toList goalVs)
+
+            pure $
+              minimum $
+                costOpenCurrent
+                  ++ catMaybes costSkipCurrent
 
 part1 :: IO Int
 part1 = do
-  valveMap <- readValveFile "sample.txt"
+  valveMap <- readValveFile "real.txt"
+  let zeroFlowValves = Map.keysSet $ Map.filter ((== 0) . flowRate) valveMap
   let absoluteFlowRate = 30 * sum (flowRate <$> Map.elems valveMap)
-  pure $ absoluteFlowRate - cost valveMap (Valve "AA") (Set.delete (Valve "AA") $ Map.keysSet valveMap) Set.empty
+  let absoluteCost = evalState (cost valveMap (Valve "AA") ((`Set.difference` zeroFlowValves) . Set.delete (Valve "AA") $ Map.keysSet valveMap) Set.empty) S {costMemo = Map.empty, distanceMemo = Map.empty}
+  traceShowM absoluteFlowRate
+  traceShowM absoluteCost
+
+  pure $ absoluteFlowRate - absoluteCost
