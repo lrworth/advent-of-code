8d07c88263c4ef2e901d21167d3a39128e29e229
Luke Worth <luke.worth@bellroy.com>
2023-01-03 10:24:07 +1100

    2022/17: Tidy up part 1 in preparation for part 2
    In particular this adds `instance Eq Chamber`.

--- 2022/17/src/Aoc.hs
+++ 2022/17/src/Aoc.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE ApplicativeDo #-}
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
@@ -8,18 +10,16 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-{-# HLINT ignore "Unused LANGUAGE pragma" #-}
-
 module Aoc where
 
-import Data.Foldable (Foldable (maximum), maximumBy)
+import Data.Finite (Finite, getFinite, modulo)
+import Data.Foldable (Foldable (maximum))
 import Data.Set qualified as Set
-import Relude.Extra.Foldable1 (Foldable1 (..), maximumOn1)
-import Relude.Extra.Tuple (fmapToSnd)
-import Relude.Unsafe qualified as Unsafe
+import Data.Vector.Sized qualified as V
+import Relude.Extra.Foldable1 (Foldable1 (..))
 
 data Jet = L | R
-  deriving (Show)
+  deriving (Eq, Show)
 
 readInputFile :: FilePath -> IO [Jet]
 readInputFile path = do
@@ -40,25 +40,44 @@ newtype Rock
       ( -- Locations of all rock parts. Positive coordinates are rightward and upward.
         Set (Int, Int)
       )
-  deriving (Show)
+  deriving (Eq, Show)
 
-rocks :: [Rock]
+rocks :: V.Vector 5 Rock
 rocks =
   Rock . Set.fromList
-    <$> [ [(0, 0), (1, 0), (2, 0), (3, 0)],
-          [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)],
-          [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
-          [(0, 0), (0, 1), (0, 2), (0, 3)],
-          [(0, 0), (1, 0), (0, 1), (1, 1)]
-        ]
+    <$> V.fromTuple
+      ( [(0, 0), (1, 0), (2, 0), (3, 0)],
+        [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)],
+        [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
+        [(0, 0), (0, 1), (0, 2), (0, 3)],
+        [(0, 0), (1, 0), (0, 1), (1, 1)]
+      )
 
-data Chamber = Chamber
+data Cycle n a = Cycle
+  { source :: V.Vector n a,
+    index :: Finite n
+  }
+  deriving (Eq, Show)
+
+mkCycle :: KnownNat n => V.Vector n a -> Cycle n a
+mkCycle source = Cycle {source, index = 0}
+
+peek :: KnownNat n => Cycle n a -> a
+peek Cycle {source, index} = V.index source index
+
+pop :: KnownNat n => State (Cycle n a) a
+pop = do
+  c@Cycle {index} <- get
+  put $ c {index = (modulo . succ . getFinite) index}
+  pure $ peek c
+
+data Chamber nj nr = Chamber
   { landedRock :: Rock,
-    jets :: [Jet],
-    fallingRocks :: [Rock],
+    jets :: Cycle nj Jet,
+    fallingRocks :: Cycle nr Rock,
     fallingRockCoord :: (Int, Int)
   }
-  deriving (Show)
+  deriving (Eq, Show)
 
 rockHeight :: Rock -> Int
 rockHeight (Rock parts) =
@@ -84,71 +103,40 @@ rockApplyOffset (offsetX, offsetY) (Rock parts) =
 rockWidth :: Rock -> Int
 rockWidth (Rock parts) = 1 + maximum (Set.map fst parts)
 
-currentFallingRock :: Chamber -> Rock
-currentFallingRock Chamber {fallingRocks} = maybe (Unsafe.head rocks) head $ nonEmpty fallingRocks
+currentFallingRock :: KnownNat nr => Chamber nj nr -> Rock
+currentFallingRock Chamber {fallingRocks} = peek fallingRocks
 
-nextFallingRocks :: Chamber -> [Rock]
-nextFallingRocks Chamber {fallingRocks} = maybe (Unsafe.tail rocks) tail $ nonEmpty fallingRocks
+chamberApplyJet :: (KnownNat nj, KnownNat nr) => State (Chamber nj nr) ()
+chamberApplyJet = modify $ \c@Chamber {landedRock, jets, fallingRockCoord} ->
+  let (jet, nextJets) = runState pop jets
+      fallingRockNewCoord@(fallingRockNewX, _) = first (applyJet jet) fallingRockCoord
+      fallingRockAbsolute = rockApplyOffset fallingRockNewCoord $ currentFallingRock c
+      fallingRockWidth = rockWidth $ currentFallingRock c
+   in if fallingRockNewX < 0 || (fallingRockNewX + fallingRockWidth) > 7 || overlaps landedRock fallingRockAbsolute
+        then c {jets = nextJets}
+        else c {fallingRockCoord = fallingRockNewCoord, jets = nextJets}
 
-chamberApplyJet :: [Jet] -> Chamber -> Chamber
-chamberApplyJet jetPattern c@Chamber {landedRock, jets, fallingRockCoord} =
-  if fallingRockNewX < 0 || (fallingRockNewX + fallingRockWidth) > 7 || overlaps landedRock fallingRockAbsolute
-    then c {jets = nextJets}
-    else c {fallingRockCoord = fallingRockNewCoord, jets = nextJets}
-  where
-    nextJets = maybe (Unsafe.tail jetPattern) tail $ nonEmpty jets
-    jet = maybe (Unsafe.head jetPattern) head $ nonEmpty jets
-    fallingRockNewCoord@(fallingRockNewX, _) = first (applyJet jet) fallingRockCoord
-    fallingRockAbsolute = rockApplyOffset fallingRockNewCoord $ currentFallingRock c
-    fallingRockWidth = rockWidth $ currentFallingRock c
+landRock :: (KnownNat nr) => State (Chamber nj nr) ()
+landRock = modify $ \c@Chamber {landedRock, fallingRocks, fallingRockCoord} ->
+  let (rock, nextRocks) = runState pop fallingRocks
+      newLandedRock =
+        joinRocks
+          landedRock
+          (rockApplyOffset fallingRockCoord rock)
+   in c
+        { landedRock = newLandedRock,
+          fallingRocks = nextRocks,
+          fallingRockCoord = (2, rockHeight newLandedRock + 3)
+        }
 
-landRock :: Chamber -> Chamber
-landRock c@Chamber {landedRock, fallingRockCoord} =
-  c
-    { landedRock = newLandedRock,
-      fallingRocks = nextFallingRocks c,
-      fallingRockCoord = (2, rockHeight newLandedRock + 3)
-    }
-  where
-    newLandedRock =
-      joinRocks landedRock
-        . rockApplyOffset fallingRockCoord
-        $ currentFallingRock c
-
-chamberApplyGravity :: Chamber -> (Chamber, Bool)
-chamberApplyGravity c@Chamber {landedRock, fallingRockCoord} =
+chamberApplyGravity :: (KnownNat nr) => State (Chamber nj nr) Bool
+chamberApplyGravity = do
+  c@Chamber {landedRock, fallingRockCoord} <- get
+  let fallingRockNewCoord@(_, fallingRockNewY) = second (subtract 1) fallingRockCoord
+      fallingRockAbsolute = rockApplyOffset fallingRockNewCoord (currentFallingRock c)
   if fallingRockNewY < 0 || overlaps landedRock fallingRockAbsolute
-    then (landRock c, True)
-    else (c {fallingRockCoord = fallingRockNewCoord}, False)
-  where
-    fallingRockNewCoord@(_, fallingRockNewY) = second (subtract 1) fallingRockCoord
-    fallingRockAbsolute = rockApplyOffset fallingRockNewCoord (currentFallingRock c)
-
-part1 :: IO Int
-part1 = do
-  jetPattern <- readInputFile "sample.txt"
-  let initialChamber =
-        Chamber
-          { landedRock = Rock Set.empty,
-            jets = jetPattern,
-            fallingRocks = rocks,
-            fallingRockCoord = (2, 3)
-          }
-  finalRock <- landedRock <$> go jetPattern (2022 :: Integer) initialChamber
-  -- putTextLn $ drawRock finalRock
-  pure $ rockHeight finalRock
-  where
-    go jetPattern landingsLeft chamber = do
-      -- print landingsLeft
-      if landingsLeft == 0
-        then pure chamber
-        else
-          let (newChamber, landed) = chamberApplyGravity $ chamberApplyJet jetPattern chamber
-           in do
-                -- print $ fallingRockCoord newChamber
-                -- putTextLn . drawRock $ currentFallingRock newChamber
-                -- putTextLn . drawRock $ landedRock newChamber
-                go jetPattern (if landed then landingsLeft - 1 else landingsLeft) newChamber
+    then True <$ landRock
+    else False <$ put (c {fallingRockCoord = fallingRockNewCoord})
 
 drawRock :: Rock -> Text
 drawRock r@(Rock parts) =
@@ -157,26 +145,89 @@ drawRock r@(Rock parts) =
     height = rockHeight r
     drawRow y = "|" <> ((\x -> if Set.member (x, y) parts then '#' else '.') <$> [0 .. 6]) <> "|"
 
--- part2 :: IO Int
--- part2 = do
---  jetPattern <- readInputFile "sample.txt"
---  let initialChamber =
---        Chamber
---          { landedRock = Rock Set.empty,
---            jets = cycle jetPattern,
---            fallingRocks = cycle rocks,
---            fallingRockCoord = (2, 3)
---          }
---  let finalRock = find ((== initialChamber) . snd) $ fmapToSnd (\n -> go (n :: Integer) initialChamber) [1 ..]
---  undefined
---  where
---    go landingsLeft chamber = do
---      -- print landingsLeft
---      if landingsLeft == 0
---        then chamber
---        else
---          let (newChamber, landed) = chamberApplyGravity $ chamberApplyJet chamber
---           in do
---                -- print $ fallingRockCoord newChamber
---                -- putTextLn . drawRock $ landedRock newChamber
---                go (if landed then landingsLeft - 1 else landingsLeft) newChamber
+part1 :: IO Int
+part1 = do
+  jetPattern <- readInputFile "real.txt"
+  V.withSizedList jetPattern $ \jetPatternV -> do
+    let initialChamber =
+          Chamber
+            { landedRock = Rock Set.empty,
+              jets = mkCycle jetPatternV,
+              fallingRocks = mkCycle rocks,
+              fallingRockCoord = (2, 3)
+            }
+    finalRock <- landedRock <$> go (10090 * 5 :: Integer) initialChamber
+    putTextLn $ drawRock finalRock
+    pure $ rockHeight finalRock
+  where
+    go landingsLeft chamber = do
+      -- print landingsLeft
+      if landingsLeft == 0
+        then pure chamber
+        else
+          let (landed, newChamber) = runState (chamberApplyJet *> chamberApplyGravity) chamber
+           in do
+                -- print $ fallingRockCoord newChamber
+                -- putTextLn . drawRock $ currentFallingRock newChamber
+                -- putTextLn . drawRock $ landedRock newChamber
+                go (if landed then landingsLeft - 1 else landingsLeft) newChamber
+
+-- -- keepFirstRows :: Int -> Rock -> Rock
+-- -- keepFirstRows n r@(Rock parts) = Rock $ Set.map (\(x, y) -> (x, y - ((topY + 1) - n))) topRows
+-- --  where
+-- --    topY = rockHeight r - 1
+-- --    topRows = Set.filter ((> topY - n) . snd) parts
+--
+-- -- landRock2 :: Chamber -> Chamber
+-- -- landRock2 c@Chamber {landedRock, fallingRockCoord} =
+-- --  c
+-- --    { landedRock = keepFirstRows 1 newLandedRock,
+-- --      fallingRocks = nextFallingRocks c,
+-- --      fallingRockCoord = (2, rockHeight newLandedRock + 3)
+-- --    }
+-- --  where
+-- --    newLandedRock =
+-- --      joinRocks landedRock
+-- --        . rockApplyOffset fallingRockCoord
+-- --        $ currentFallingRock c
+-- --
+-- -- chamberApplyGravity2 :: Chamber -> (Chamber, Bool)
+-- -- chamberApplyGravity2 c@Chamber {landedRock, fallingRockCoord} =
+-- --  if fallingRockNewY < 0 || overlaps landedRock fallingRockAbsolute
+-- --    then (landRock2 c, True)
+-- --    else (c {fallingRockCoord = fallingRockNewCoord}, False)
+-- --  where
+-- --    fallingRockNewCoord@(_, fallingRockNewY) = second (subtract 1) fallingRockCoord
+-- --    fallingRockAbsolute = rockApplyOffset fallingRockNewCoord (currentFallingRock c)
+-- --
+-- -- {-
+-- -- Ideas:
+-- --- https://en.wikipedia.org/wiki/Cycle_detection
+-- --- These algorithms depend on a function applied to a finite set. The chamber grows indefinitely so it is not a finite set. Thus it needs to be culled. Except in pathological cases, this is possible by identifying
+-- --- Tortoise and hare algorithm might work
+-- ---}
+-- -- part2 :: IO Int
+-- -- part2 = do
+-- --  jetPattern <- readInputFile "sample0.txt"
+-- --  let initialChamber =
+-- --        Chamber
+-- --          { landedRock = Rock Set.empty,
+-- --            jets = jetPattern,
+-- --            fallingRocks = rocks,
+-- --            fallingRockCoord = (2, 3)
+-- --          }
+-- --  let finalRock = find ((== initialChamber) . snd . traceShowId) $ fmapToSnd (\n -> go jetPattern (n :: Integer) initialChamber) [1 ..]
+-- --  print finalRock
+-- --  undefined
+-- --  where
+-- --    go jetPattern landingsLeft chamber =
+-- --      -- traceShow landingsLeft $
+-- --      -- print landingsLeft
+-- --      if landingsLeft == 0
+-- --        then chamber
+-- --        else
+-- --          let (newChamber, landed) = chamberApplyGravity2 $ chamberApplyJet jetPattern chamber
+-- --           in do
+-- --                -- print $ fallingRockCoord newChamber
+-- --                -- putTextLn . drawRock $ landedRock newChamber
+-- --                go jetPattern (if landed then landingsLeft - 1 else landingsLeft) newChamber
